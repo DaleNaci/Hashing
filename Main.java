e658af37e579854aae65620d782d047c5e2f6ae6
DaleNaci <dtn22@njit.edu>
2023-04-17 03:35:21 -0400

    Translated to Java with the exception that 0's aren't handled properly

---
+++ Main.java
@@ -0,0 +1,220 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Please provide the file name as a command line argument!");
+            System.exit(1);
+        }
+
+        String file_name = args[0];
+        FileParser parser = new FileParser();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file_name))) {
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                parser.parseLine(line.trim());
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        }
+    }
+}
+
+
+class FileParser {
+    private Lexicon lex;
+
+    public FileParser() {
+        this.lex = new Lexicon();
+    }
+
+    public void parseLine(String line) {
+        int command = Integer.parseInt(line.substring(0, 2));
+        String info = null;
+        
+        if (line.length() > 2) {
+            info = line.substring(3);
+        }
+
+        if (command == 10) {
+            lex.insert(info);
+        } else if (command == 11) {
+            lex.delete(info);
+        } else if (command == 12) {
+            lex.search(info);
+        } else if (command == 13) {
+            lex.printLex();
+        } else if (command == 14) {
+            lex.create(Integer.parseInt(info));
+        } else if (command == 15) {
+            // Do nothing, 15 means it is a comment
+        }
+    }
+}
+
+
+class Lexicon {
+    private int slots;
+    private int[] table;
+    private char[] array;
+    
+    public Lexicon() {
+        this.slots = 0;
+        this.table = new int[0];
+        this.array = new char[0];
+    }
+    
+    public void create(int slots) {
+        this.slots = slots;
+        this.table = new int[slots];
+        this.array = new char[15 * slots];
+        for (int i = 0; i < this.array.length; i++) {
+            this.array[i] = ' ';
+        }
+    }
+    
+    public void insert(String word) {
+        int hash_val = this.__hash(word, 0);
+        int table_slot = this.__get_slot(hash_val);
+        int array_idx = this.__get_array_idx();
+        
+        this.table[table_slot] = array_idx;
+        this.__add_to_array(word, array_idx);
+    }
+    
+    public void delete(String word) {
+        int idx = new String(this.array).indexOf(word);
+        
+        for (int i = 0; i < word.length(); i++) {
+            this.array[idx] = '*';
+            idx++;
+        }
+        
+        int slot = this.__hash(word, 0);
+        this.table[slot] = 0;
+        
+        System.out.printf("%s\tdeleted from slot %d%n", word, slot);
+    }
+    
+    public void search(String word) {
+        boolean word_found = new String(this.array).contains(word);
+        
+        if (word_found) {
+            int slot = new String(this.array).indexOf(word);
+            System.out.printf("%s\tfound at slot %d%n", word, slot);
+        } else {
+            System.out.printf("%s\tnot found%n", word);
+        }
+    }
+    
+    public boolean isEmpty() {
+        for (int val : this.table) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean isFull() {
+        for (int val : this.table) {
+            if (val == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public void printLex() {
+        System.out.println("T:\n");
+        this.__print_table();
+        
+        System.out.print("\nA: ");
+        this.__print_array();
+        
+        System.out.println();
+    }
+    
+    private void __print_table() {
+        for (int i = 0; i < this.table.length; i++) {
+            if (this.table[i] == 0) {
+                System.out.printf("%d:%n", i);
+            } else {
+                System.out.printf("%d: %d%n", i, this.table[i]);
+            }
+        }
+    }
+    
+    private void __print_array() {
+        for (char val : this.array) {
+            System.out.print(val);
+        }
+    }
+    
+    private int __get_array_idx() {
+        int idx = 0;
+        
+        while (this.array[idx] != ' ') {
+            idx++;
+        }
+        
+        return idx;
+    }
+    
+    private void __add_to_array(String word, int start_idx) {
+        int idx = start_idx;
+        
+        for (char c : word.toCharArray()) {
+            this.array[idx] = c;
+            idx++;
+        }
+        
+        this.array[idx] = '\\';
+    }
+
+
+    private int __get_slot(int start_idx) {
+        int idx = start_idx;
+    
+        while (table[idx] != 0) {
+            idx++;
+    
+            if (idx >= table.length) {
+                __double_table_size();
+            }
+        }
+    
+        return idx;
+    }
+    
+    private void __double_table_size() {
+        int[] newTable = new int[slots * 2];
+        char[] newArray = new char[15 * slots * 2];
+    
+        for (int i = 0; i < table.length; i++) {
+            newTable[i] = table[i];
+        }
+    
+        for (int i = 0; i < array.length; i++) {
+            newArray[i] = array[i];
+        }
+    
+        slots *= 2;
+        table = newTable;
+        array = newArray;
+    }
+    
+    private int __hash(String word, int i) {
+        int ascii_total = 0;
+        for (int j = 0; j < word.length(); j++) {
+            ascii_total += (int) word.charAt(j);
+        }
+    
+        return (((ascii_total - 4) % slots) + i * i) % slots;
+    }
+}
--- file_parser.py
+++ file_parser.py
@@ -10,24 +10,18 @@ class FileParser:
         command = int(line[:2])
 
         info = None
-
         if len(line) > 2:
             info = line[3:]
 
         if command == 10:
             self.lex.insert(info)
-
         elif command == 11:
             self.lex.delete(info)
-
         elif command == 12:
             self.lex.search(info)
-
         elif command == 13:
             self.lex.print_lex()
-
         elif command == 14:
             self.lex.create(int(info))
-
         elif command == 15:
             pass # Do nothing, 15 means it is a comment
